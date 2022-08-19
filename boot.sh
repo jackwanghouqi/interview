e824c94b70b2dcc4fd8ec44ad207840181baa535
johnson <Johnsonwanghouqi@gmail.com>
2022-08-18 22:28:41 -0400

    interview

--- boot.sh
+++ boot.sh
@@ -2,4 +2,5 @@ ps aux|grep app.App|grep springframework|awk '{print $2}'|xargs kill -9
 export PATH="${JAVA_HOME}/bin:$PATH:${JAVA_HOME}/bin"
 echo $PATH
 chmod 777 gradlew
+#gradle wrapper --gradle-version=5.1.1
 ./gradlew --stacktrace bootRun
