cd37a0bc533969cea5153e7b2862408bfe16f3c1
johnson <Johnsonwanghouqi@gmail.com>
2022-08-18 21:47:26 -0400

    interview

--- boot.sh
+++ boot.sh
@@ -1,5 +1,4 @@
 ps aux|grep app.App|grep springframework|awk '{print $2}'|xargs kill -9
-export JAVA_HOME="/opt/jdk-10.0.1"
 export PATH="${JAVA_HOME}/bin:$PATH:${JAVA_HOME}/bin"
 echo $PATH
 chmod 777 gradlew
